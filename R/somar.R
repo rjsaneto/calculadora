bf30fb847491d48eb16c6f329d69ab437689b7f0
Raymundo Sá-Neto <rjsaneto@uesb.edu.br>
2024-02-26 12:01:32 -0300

    first commit

---
+++ R/somar.R
@@ -0,0 +1,9 @@
+#' Função Somar
+#' Esta função faz a soma de dois valores
+#'@usage a um número para somar
+#'@usage b outro número para somar
+#'@examples somar(3,4)
+#'@export
+somar<-function(a,b){
+  a+b
+}
---
+++ R/subtrair.R
@@ -0,0 +1,9 @@
+#' Função Subtrair
+#' Esta função subtrai a soma de dois valores
+#'@usage a um número para subtrair
+#'@usage b outro número para subtrair
+#'@examples subtrair(3,4)
+#'@export
+subtrair<-function(a,b){
+  a-b
+}
